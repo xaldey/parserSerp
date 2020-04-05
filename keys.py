907f1665850b56cf3fd90c62a2153e2b3d2f9763
xaldey <fguzkp25@gmail.com>
2020-04-05 17:08:12 +0300

    Прогнал flake8 и исправил большинство ошибок

--- keys.py
+++ keys.py
@@ -1,6 +1,4 @@
-
 ROOT_URL_YA = 'https://yandex.ru/search/?'
-# ROOT_URL_GOOGLE = 'https://google.com/search/?'
-# URL_MAILRU = 'https://go.mail.ru/search?q=seat&fm=1'
-HEADERS = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 '
-                        'Safari/537.36'}
+HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/80.0.3987.132 '
+                         'Safari/537.36'}
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup as bs
-import csv, json, requests, datetime
+import csv
+import requests
+import datetime
 from keys import ROOT_URL_YA, HEADERS
 
 date_stamp = datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
@@ -9,18 +11,17 @@ recursive_search_array = []
 
 
 def main():
-    # Сначала ищем в Яндекс
     serp_string = str(input('Что ищем?: '))
     user_inputs.append(serp_string)
     serp_results = int(input('Сколько результатов найти?: '))
     user_inputs.append(serp_results)
-    rec_search = input('Рекурсия? Укажи True or False')
-    # rec_search = True
+    rec_search = input('Рекурсия? Укажи True or False ')
     user_inputs.append(rec_search)
-    search_url = ROOT_URL_YA + 'text=' + serp_string + '&lr=' + str(serp_results)
+    search_url: str = ROOT_URL_YA + "text=" + serp_string + "&lr=" + \
+                      str(serp_results)
     user_inputs.append(search_url)
-    result_type = input('Укажите конечный формат для результата: json, csv, console ')
-    # result_type = 'csv'
+    result_type = input('Укажите конечный формат результата: '
+                        'json, csv, console ')
     user_inputs.append(result_type)
     filename = str('result--' + date_stamp + '.' + result_type)
     user_inputs.append(filename)
@@ -35,7 +36,6 @@ def search():
     print('*' * 100)
     print('Осуществляем поиск по ссылке: ' + user_inputs[3])
     response = requests.get(user_inputs[3], headers=HEADERS)
-    # response.encoding = 'cp1251'
     if response.ok:
         soup = bs(response.text, 'html.parser')
         li = soup.find_all('li', class_='serp-item')
@@ -48,7 +48,8 @@ def search():
                 search_links.append(result_link)
             else:
                 break
-        print('Длина массива результатов первичного поиска: ' + str(len(search_links)))
+        print('Длина массива результатов первичного поиска: '
+              + str(len(search_links)))
         print('*' * 100)
         print('Первичный поиск завершен!')
         print('*' * 100)
@@ -60,12 +61,11 @@ def search():
 def recursive_search():
     for link in search_links:
         look_for = link[1]
-        print('Парсим сайт для рекурсивного поиска: ' + look_for)
+        print('Парсим сайт для рекурсивного поиска: '
+              + look_for)
         recursive_response = requests.get(look_for, headers=HEADERS)
         if recursive_response.ok:
             recursive_soup = bs(recursive_response.text, 'html.parser')
-            # a_list = recursive_soup("a")
-            # print(a_list)
             for el in recursive_soup.findAll('a'):
                 link = str(el.get('href'))
                 name = str(el.get_text())
@@ -80,8 +80,10 @@ def recursive_search():
 def export_results():
     if user_inputs[4] == 'csv':
         filename = user_inputs[5]
-        csv.writer(open(filename, 'w', newline='', encoding='utf-8')).writerows(search_links)
-        csv.writer(open(filename, 'w', newline='', encoding='utf-8')).writerows(recursive_search_array)
+        csv.writer(open(filename, 'w', newline='', encoding='utf-8')) \
+            .writerows(search_links)
+        csv.writer(open(filename, 'w', newline='', encoding='utf-8')) \
+            .writerows(recursive_search_array)
     elif user_inputs[4] == 'json':
         print('Пока не работает :(' + '\n' + (' #' * 50))
     else:
@@ -99,6 +101,3 @@ if __name__ == '__main__':
     else:
         print('Первичный поиск завершен.')
     export_results()
-
-
-
